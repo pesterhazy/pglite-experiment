dbbc71569911545c82d8e549e06b478f99396337
Paulus Esterhazy <pesterhazy@gmail.com>
2024-04-02 13:29:10 +0200

    WIP

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,10 +2,55 @@ import { test } from "node:test";
 import assert from "node:assert/strict";
 import { PGlite } from "@electric-sql/pglite";
 
-let db: PGlite | undefined;
+class KVClient {
+  db: PGlite;
+
+  constructor(db: PGlite) {
+    this.db = db;
+  }
+
+  async get(k) {
+    await this.db.query(`
+drop table if exists kvpairs;
+`);
+    await this.db.query(`
+create table kvpairs (
+  k text primary key,
+  v text not null
+)`);
+
+    await this.db.query(`
+insert into kvpairs
+(k,v)
+VALUES
+('foo','bar'),
+('foo2','bar2')
+`);
+    let result = await this.db.query("select v from kvpairs where k=$1", [k]);
+    return result.rows[0].v;
+  }
+}
+
+// *****************
+
+let globalDb: PGlite | undefined;
 
 test(async () => {
-  if (!db) db = new PGlite();
-  let result = (await db.query("select 1 as v;")) as any;
+  if (!globalDb) globalDb = new PGlite();
+  let result = (await globalDb.query("select 1 as v;")) as any;
   assert.equal(result.rows[0].v, 1);
+});
+
+test(async () => {
+  if (!globalDb) globalDb = new PGlite();
+  let kvclient = new KVClient(globalDb);
+  let result = await kvclient.get("foo");
+  assert.equal(result, "bar");
+});
+
+test(async () => {
+  if (!globalDb) globalDb = new PGlite();
+  let kvclient = new KVClient(globalDb);
+  let result = await kvclient.get("foo2");
+  assert.equal(result, "bar2");
 });
